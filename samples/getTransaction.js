52208bba27563ad123cc4430ac9fedf2c6723a59
Damyan Stanchev <wyand@abv.bg>
2019-01-16 19:52:09 +0200

    Get transaction updates

--- samples/getTransaction.js
+++ samples/getTransaction.js
@@ -1,11 +1,19 @@
-const { web3, abiUtilities } = require('./config');
+const {
+	web3,
+	abiUtilitiesV1,
+	abiCreativesV1,
+	abiMembersV1
+} = require('./config');
 
 const abiDecoder = require('abi-decoder');
-abiDecoder.addABI(abiUtilities);
+abiDecoder.addABI(abiUtilitiesV1);
+abiDecoder.addABI(abiCreativesV1);
+abiDecoder.addABI(abiMembersV1);
 
-web3.eth.getTransaction('0x19359f20c32f12779aca447a615aa2fcac8d579ae65553f9ff0ba76d571c4276').then(
+web3.eth.getTransaction(process.argv[2]).then(
 	function(dataRaw) {
 		var data = abiDecoder.decodeMethod(dataRaw.input);
-		console.log(data, dataRaw.input);
+		console.log(dataRaw.input);
+		console.log(data);
 	}
 );
